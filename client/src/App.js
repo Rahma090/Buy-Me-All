2e10f42a9f5edc6369539353771351db22451afc
oussemacherif <ousseemacherif@gmail.com>
2023-12-21 14:14:35 +0100

    some changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import ECommerceHomePage from './component/ECommerceHomePage/ECommerceHomePage.jsx';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import SingnIn from './component/SingnIn/SingnIn.jsx';
-import SignUp from './component/SingnUp/SignUp.jsx';
+import SignIn from './component/SignIn/SignIn.jsx';
+import SignUp from './component/SignUp/SignUp.jsx';
 import ProductDetails from './component/ProductDetails/ProductDetails.jsx';
 
 function App() {
@@ -10,8 +10,8 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path='/ECommerceHomePage' element={ <ECommerceHomePage/>}></Route>
-      <Route path='/SingnIn' element={ <SingnIn/>}></Route>
-      <Route path='/SingnUp' element={<SignUp/>}></Route>
+      <Route path='/SignIn' element={ <SignIn/>}></Route>
+      <Route path='/SignUp' element={<SignUp/>}></Route>
       <Route path='/ProductDetails' element={<ProductDetails/>} ></Route>
    
 
